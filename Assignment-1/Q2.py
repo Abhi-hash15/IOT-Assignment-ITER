ee590d204cf02ba6afcaf800656c4a5405f25c81
abhinav kumar <abhijnv152006@gmail.com>
2024-10-07 11:54:41 +0530

    updating the answer

--- Assignment-1/Q2.py
+++ Assignment-1/Q2.py
@@ -1,6 +1,9 @@
-weight=float(input("enter weight(in kg): "))
-height=float(input("enter height(in mt): "))
-pd=0.45359237*weight
-inch=0.0254*height
-bmi=pd/inch*inch
-print(bmi)
+dist=float(input("Enter distance between country(in km): "))
+meters = dist*1000
+feet = dist*3280.84
+inches = dist*39370.1
+cm = dist*100000
+print(meters)
+print(feet)
+print(inches)
+print(cm)
--- Assignment-1/Q6.py
+++ Assignment-1/Q6.py
@@ -1,5 +1,5 @@
 min=int(input("enter minutes: "))
-d=min//1400
+d=min//1440
 yr=min//525600
 print(min,"min is ",d," day")
 print(min,"min is ",yr," year")
